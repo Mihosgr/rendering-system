03e71c7edccaf70b64b2040a79689e44cab739ac
Mihosgr <mihosgr64@gmail.com>
2026-09-01 03:51:41 +0300

    Initial commit - Spring Boot structure

---
+++ src/main/java/com/mikedvl/rendering/RenderingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mikedvl.rendering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenderingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RenderingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mikedvl/rendering/RenderingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikedvl.rendering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
